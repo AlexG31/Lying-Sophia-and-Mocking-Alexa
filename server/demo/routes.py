a5a1ed6a593ffc4b4539389c82a0228a8aaf057c
Alex G <AresEyE@163.com>
2019-10-20 17:12:53 +0800

    server: seq play

--- server/demo/routes.py
+++ server/demo/routes.py
@@ -1,6 +1,69 @@
 from demo import app
+from flask import request
+from collections import deque
+import time
+
+speaker = 'Sophia'
+keys = deque(maxlen = 100)
+scriptIndexDict = dict()
+validNames = set(['Alexa', 'Sophia'])
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/ask',methods=['GET',])
+def ask():
+    name = request.args.get('name')
+    password = request.args.get('password')
+
+    if name in validNames:
+        speaker, index = getState(password)
+        while speaker != name:
+            time.sleep(1)
+            speaker, index = getState(password)
+
+        return str(index)
+
+    return "-1"
+
+def getState(pw):
+    global keys
+    global scriptIndexDict
+    if pw not in scriptIndexDict:
+        if len(keys) >= keys.maxlen:
+            p = keys.popleft()
+            del scriptIndexDict[p]
+        keys.append(pw)
+        scriptIndexDict[pw] = ('Sophia', 0)
+    return scriptIndexDict[pw]
+
+def setNextState(name, pw):
+    global keys
+    global scriptIndexDict
+    if pw not in scriptIndexDict:
+        if len(keys) >= keys.maxlen:
+            p = keys.popleft()
+            del scriptIndexDict[p]
+        keys.append(pw)
+        scriptIndexDict[pw] = ('Sophia', 0)
+    
+    speaker, index = scriptIndexDict[pw]
+    nextName = findNextSpeaker(name)
+    if speaker == name:
+        scriptIndexDict[pw] = (nextName, index + 1)
+    return None
+
+def findNextSpeaker(name):
+    for n in validNames:
+        if n != name:
+            return n
+    return None
+
+@app.route('/report',methods=['GET',])
+def report():
+    name = request.args.get('name')
+    password = request.args.get('password')
+    setNextState(name, password)
+    return "Hello, report!"
