ba4c1ca2af483af6147640a656527184fb1a76a7
Alex G <AresEyE@163.com>
2019-10-27 19:10:06 +0800

    change sentences folder

--- bin/union-sentences.sh
+++ bin/union-sentences.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 echo '======== GPT-2 Model =========='
 gitFolder=$1
-sentenceFolder=$2
-outputPath=$3
+outputPath=$2
 
 seed_path=$gitFolder/seed/conversation1.txt
 # union sentences according to script order
-python3 $gitFolder/model/util.py --cmd union --seed_path $seed_path \
-    --in_folder $sentenceFolder \
+python3 $gitFolder/model/util.py --cmd union \
+    --seed_path $seed_path \
+    --in_folder $gitFolder/sentences \
     --out_json_path $outputPath
